f94af7d858f3f6d406fe3b252ea4bcb1699e4170
vinod <vinodk2757@gmail.com>
2024-02-01 10:39:29 +0530

    golang env added

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+  "os"
+  "log"
+  "github.com/lpernett/godotenv"
   "github.com/gin-gonic/gin"
   "golang-gin-boilerplate/routes"
 )
 
 func main() {
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+  port := os.Getenv("PORT")
   router := gin.Default()
   routes.HealthRoute(router)
-  var port = "3001"
   router.Run("0.0.0.0:" + port)
 }
