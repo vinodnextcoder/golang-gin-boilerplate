41224391afd9e54cc02e7e253a631829eed4ee6a
vinod <vinodk2757@gmail.com>
2024-02-15 18:16:00 +0530

    swagger docs

--- controllers/health/health.go
+++ controllers/health/health.go
@@ -1,17 +1,29 @@
 package health
 
 import (
-  "net/http"
-  "github.com/gin-gonic/gin"
-  "github.com/sirupsen/logrus"
-  "golang-gin-boilerplate/services/logservice"
+	"golang-gin-boilerplate/services/logservice"
+	"net/http"
+
+	_ "golang-gin-boilerplate/docs"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 )
 
+// helloCall godoc
+// @Summary hellow example
+// @Schemes
+// @Description Hello
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} Hello, You created a Web App!
+// @Router /health [get]
 func Health() gin.HandlerFunc {
 	return func(c *gin.Context) {
-    logservice.Info("Health endpoint called", logrus.Fields{"status":"200","functionName":"Health", "controller":"healthcontroller"})
-    c.JSON(http.StatusOK, gin.H{
-      "message": "Ok",
-    })
+		logservice.Info("Health endpoint called", logrus.Fields{"status": "200", "functionName": "Health", "controller": "healthcontroller"})
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Ok",
+		})
 	}
 }
